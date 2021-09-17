cc50b4d3b5eab316b918d166635755000824d9ea
Carlos <itscarlosisaac@gmail.com>
2021-09-17 12:24:19 -0400

    Fixed typo in nginx for client.

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>Hello Front End</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
           <Link to="/page">Page</Link>
